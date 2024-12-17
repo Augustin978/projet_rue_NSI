ec4c97fcc9c5e36ae07cda77d61c823160eb5571
Augustin978 <augustin.fournis@eleves.ecmorlaix.fr>
2024-12-17 16:34:30 +0000

    rue

---
+++ ma_rue.py
@@ -0,0 +1,25 @@
+# Dépendance
+from ipycanvas import Canvas 
+
+
+
+# Définition 
+
+# Création d'un canvas nommé rue de 800 pixels de large par 400 pixels de haut
+
+
+# Définition d'une fonction d'affichage
+rue = Canvas(width = 200, height = 160)
+def affiche(rue) :
+    '''
+    Efface et affiche le canvas dans le notebook
+    '''
+    rue.clear()
+    rue
+
+
+
+# Test
+
+# Affichage du canvas rue pour un test
+affiche(rue)
