33b2cb3f1c8e89b6cefd363eaaf32d6bb2e36cd5
Racs6 <147829820+Racs6@users.noreply.github.com>
2024-12-19 09:32:25 +0000

    v0.1

---
+++ toit.py
@@ -0,0 +1,29 @@
+# Dépendances
+from ma_rue import rue, affiche 
+# Définitions
+
+# Fonction toit1()
+def toit1(x, niveau):
+    '''
+    Dessine un triangle plein de couleur noir de 40 pixels de haut
+    et avec une base de 160 pixels 
+    Paramètres :
+        x : abcisse du centre du toit
+        niveau : numero du niveau (0 pour les rdc, ...)
+    '''
+    y = rue.height - niveau * 60 # ordonnée de la base du toit
+    
+    
+    
+    
+    
+    
+       
+    
+# Tests
+affiche(rue)
+toit1(rue.width/2, 0)
+# Autres tests
+for i in range(5) :
+    for j in range(1, 6) :
+        toit1(0 + 200 * i, j)
