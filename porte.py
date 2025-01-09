d0bbbe933f14a6a785e197fde6ca1b8daa7af2c8
Augustin978 <augustin.fournis@eleves.ecmorlaix.fr>
2025-01-09 11:54:16 +0100

    Add files via upload

---
+++ porte.py
@@ -0,0 +1,43 @@
+# Dépendances
+from ma_rue import rue, affiche
+from rectangle import rectangle
+from trait import trait
+from couleur_aleatoire import couleur_aleatoire
+from math import pi
+from random import randint
+# Définitions
+
+# Fonction portes()
+def portes(x,y):
+    '''
+    Dessine une porte de 50 pixels en largeur et 70 pixels en hauteur
+    La forme du haut de la porte est aléatoirement rectangulaire ou arrondi
+    La couleur pleine de remplissage est choisi aléatoirement parmi les couleurs HTML valides
+    Paramètres :
+        x est l'abcisse du milieu de la base de la porte
+        y est l'ordonnée du sol du niveau de la porte        
+    '''     
+
+    augustin= randint(1,2)
+
+    if augustin == 1 :
+        rectangle(x,y,50,70,couleur_aleatoire())
+    
+    if augustin == 2 :
+        
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+# Tests
+affiche(rue)
+
+for i in range(21) :
+    portes(0 + i * 40,rue.height)
