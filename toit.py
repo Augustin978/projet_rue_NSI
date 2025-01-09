0352cb54ba4c4e1a9f111a6a8c2b06292ace190b
Augustin978 <augustin.fournis@eleves.ecmorlaix.fr>
2025-01-09 11:07:47 +0100

    Add files via upload

--- toit.py
+++ toit.py
@@ -1,31 +1,35 @@
 # Dépendances
-from ma_rue import rue, affiche 
+from ma_rue import rue, affiche
+from random import randint
+from toit1 import toit1
+from toit2 import toit2
+# Définitions
 
-# Fonction toit1()
-def toit1(x, niveau):
+# Fonction toits()
+
+def toits(x, niveau):
     '''
-    Dessine un triangle plein de couleur noir de 40 pixels de haut
-    et avec une base de 160 pixels 
-    Paramètres :
-        x : abcisse du centre du toit
-        niveau : numero du niveau (0 pour les rdc, ...)
+    Dessine aléatoirement un toit plat ou un toit en pointe
+    à l'ordonnée du niveau passé en paramètre
+    Paramètres
+        x : abscisse du centre de l'étage
+        y_sol: ordonnée du sol
+        niveau : numéro de l'étage en partant de 0 pour le rdc
     '''
     y = rue.height - niveau * 60 # ordonnée de la base du toit
-    rue.stroke_style = 'black'
-    rue.fill_style = 'black'
-    rue.begin_path()
-    rue.move_to(x-80, y)
-    rue.line_to(x, y-40)
-    rue.line_to(x+80, y)
-    rue.close_path()
-    rue.fill()
+    oscar=randint(1,2)
+    if oscar == 1 :
+        toit1(x,y)
+    if oscar == 2 :
+        toit2(x,y)
+
+    
+        
+    
     
     
 # Tests
-if __name__ == '__main__':
-    affiche(rue)
-    toit1(rue.width/2, 0)
-    # Autres tests
-    for i in range(5) :
-        for j in range(1, 6) :
-            toit1(0 + 200 * i, j)
+affiche(rue)
+for i in range(5) :
+    for j in range(6) :
+        toits(0 + 200 * i, j)
