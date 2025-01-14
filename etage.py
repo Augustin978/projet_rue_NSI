00b2ea6dea3c647aee174bd7785a30f01eb41d0f
kri-ro <sviatoslav.kirsanov@eleves.ecmorlaix.fr>
2025-01-14 12:26:29 +0000

    geg

---
+++ etage.py
@@ -0,0 +1,42 @@
+# Dépendances
+from ma_rue import rue, affiche
+from facade import facade
+from fenetre import fenetre
+from balcon import balcon 
+
+from random import randint
+
+
+# Définitions
+
+# Fonction etage()
+def etage(x, couleur, niveau):
+    affiche(rue)
+couleur = couleur_aleatoire()
+for n in range(6) :
+    facade(rue.width/2, couleur, n)
+    '''
+    Dessine sur une facade un étage avec 3 éléments choisis aléatoirement
+    parmi une fenêtre ou une porte fenêtre avec balcon.  
+    
+    Paramètres
+        x : abscisse du milieu de la base de l'étage
+        couleur : couleur fixée par l'immeuble
+        niveau : numéro de l'étage en partant de 0 pour le rdc
+    '''
+    y = rue.height - niveau * 60 # ordonnée de la base de l'etage
+    
+    # Murs
+    
+        
+    
+    # Eléments
+    
+
+
+    # Tests
+from couleur_aleatoire import couleur_aleatoire
+affiche(rue)
+couleur = couleur_aleatoire()
+for n in range(6) :
+    etage(rue.width/2,couleur,n)
